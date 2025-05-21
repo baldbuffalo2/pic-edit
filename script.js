cd7e57c089ffa19fb569b5c841ee78af5f79692d
baldbuffalo2 <111616558+baldbuffalo2@users.noreply.github.com>
2025-05-22 03:41:31 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const img_file = document.getElementById('img-file');
+const gender = document.querySelector('input[name="gender"]:checked').value;
+const undres_div = document.getElementById("img-result");
+const app_id = "e8b1c4f3cf13492d9229f7e3f2d7ab13";
+const model_id = "UndressPicApp-all-scopes";
